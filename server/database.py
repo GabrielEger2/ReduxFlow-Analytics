63ebf3a3936e612d73655d2265c48fd6eb5a9f06
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-01-27 18:56:47 -0300

    style: lint run and database files

--- server/database.py
+++ server/database.py
@@ -1,3 +1,9 @@
+"""Database module for Flask application.
+
+This module initializes an SQLAlchemy database instance that can be used
+throughout the Flask application for handling database operations.
+"""
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
--- server/run.py
+++ server/run.py
@@ -1,9 +1,18 @@
+"""Flask application creation and configuration.
+
+This module defines the Flask application instance and its configuration,
+including API settings, blueprints, database connection, and Swagger UI setup.
+It provides the `create_app` function to initialize and configure the Flask app.
+"""
+
 from flask import Flask
-from flask_smorest import Api
+# from flask_smorest import Api
 
 from database import db
 
+
 def create_app(db_url=None):
+    """Create and configure the Flask application."""
     app = Flask(__name__)
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
@@ -17,13 +26,14 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
-    api = Api(app)
+    # api = Api(app)
 
     with app.app_context():
         db.create_all()
 
     return app
 
+
 if __name__ == "__main__":
-    app = create_app()
-    app.run(port=5000, debug=True)
+    server = create_app()
+    server.run(port=5000, debug=True)
