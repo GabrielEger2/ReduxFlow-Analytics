6d6cc29d843376b42b46b29627df9bb683958b65
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-02-02 14:16:52 -0300

    feat: add login page and component

---
+++ client/src/components/LogInRegister.tsx
@@ -0,0 +1,69 @@
+// @ts-expect-error - ignore typescript error
+const LogInRegister = ({ formType, handleFormTypeChange }) => {
+  return (
+    <div className="card-body w-full">
+      <h2 className="card-title">
+        {formType === 'login' ? 'LogIn' : 'Register'}
+      </h2>
+      <label className="form-control w-full">
+        <div className="label">
+          <span className="label-text">Email</span>
+        </div>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full"
+        />
+      </label>
+      <label className="form-control w-full">
+        <div className="label">
+          <span className="label-text">Password</span>
+        </div>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full"
+        />
+      </label>
+      {formType === 'register' && (
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">Confirm Password</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+        </label>
+      )}
+      {formType === 'login' && (
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">Remember me</span>
+            <input type="checkbox" className="checkbox checkbox-primary" />
+          </label>
+        </div>
+      )}
+      <div className="form-control">
+        <div className="label">
+          <span
+            onClick={handleFormTypeChange}
+            className="label-text underline cursor-pointer"
+          >
+            {formType === 'login'
+              ? 'First time here? Create an account'
+              : 'Already have an account? Log in'}
+          </span>
+        </div>
+      </div>
+      <div className="card-actions justify-center mt-2">
+        <button className="btn btn-primary w-full text-base-100">
+          {formType === 'login' ? 'LogIn' : 'Register'}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default LogInRegister
--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -1,7 +1,57 @@
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+
+import LogInRegister from '../components/LogInRegister'
+
 const LogIn = () => {
-    return (
-        <div>LogIn</div>
-    )
+  const [isLogin, setIsLogin] = useState(false)
+  const [isAnimationComplete, setIsAnimationComplete] = useState(false)
+
+  const handleFormTypeChange = () => {
+    setIsLogin(!isLogin)
+    setIsAnimationComplete(false) // Reset animation completion flag
+  }
+
+  const handleAnimationComplete = () => {
+    setIsAnimationComplete(true)
+  }
+
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <div className="card card-side bg-base-100 shadow-xl">
+        <div className="absolute h-full">
+          <motion.img
+            className="object-cover w-[73%] h-full"
+            src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            alt="Album"
+            style={{ userSelect: 'none' }}
+            initial={{ x: 0 }}
+            animate={{ x: isLogin ? '100%' : 0 }}
+            transition={{ duration: 0.5 }}
+            onAnimationComplete={handleAnimationComplete} // Set the onAnimationComplete callback
+          />
+        </div>
+        <div className="flex justify-center">
+          <div
+            className={`w-full flex ${isLogin && isAnimationComplete ? 'z-10' : ''}`}
+          >
+            <LogInRegister
+              formType="register"
+              handleFormTypeChange={handleFormTypeChange}
+            />
+          </div>
+          <div
+            className={`w-full flex ${!isLogin && isAnimationComplete ? 'z-10' : ''}`}
+          >
+            <LogInRegister
+              formType="login"
+              handleFormTypeChange={handleFormTypeChange}
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default LogIn
